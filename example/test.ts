d9ee3c5c159988e21152cd026d88fb41efd38fa4
letitgobaby <48303144+letitgobaby@users.noreply.github.com>
2021-08-21 13:25:13 +0900

    add typescript

--- example/test.ts
+++ example/test.ts
@@ -1,8 +1,7 @@
-// var dayjs = require("dayjs");
-var CalAPI = require('../dist/index');
+import { default as CalAPI } from '../dist/index';
 
 
-var tempHoliday = [
+const tempHoliday = [
   {
     dateName: "1월 1일",
     month: 1,
@@ -82,21 +81,8 @@ var tempHoliday = [
   },
 ];
 
-var cal = new CalAPI({
+
+const cal = new CalAPI({
   holidayList: tempHoliday,
   lunar: true
 });
-
-console.time("year");
-// for (var i = 0; i < 12*50; i++) {
-//   cal.yearCalendar(2021 + i);
-// }
-
-console.log( cal.monthCalendar(2021, 2) );
-
-// console.log( cal.yearCalendar(2021));
-
-// console.log( yearHoliday(2021, tempHoliday));
-// console.log( cal.yearHoliday(2021, tempHoliday) );
-
-console.timeEnd("year");
---
+++ src/index.ts
@@ -0,0 +1,216 @@
+import dayjs from 'dayjs';
+import "./dayjs-lunar.js";
+
+declare module "dayjs" {
+  interface Dayjs {
+    lunar(): dayjs.Dayjs;
+    solar(): dayjs.Dayjs;
+  }
+}
+
+interface HolidayType {
+  dateName: string;
+  month: number;
+  date: number;
+  isLunar: boolean;
+  bufferDay: boolean;
+}
+
+interface ConfigType {
+  holidayList: Array<HolidayType>;
+  lunar: boolean;
+}
+
+interface DateObjectType {
+  year: number;
+  month: number;
+  date: number,
+  dayOfWeek: number;
+  holidayName: string;
+  fullDate: string;
+  lunarDate: string; 
+}
+
+
+class CalAPI {
+  readonly holidayList: Array<HolidayType>;
+  readonly lunar: Boolean;
+
+  constructor(config: ConfigType) {
+    this.holidayList = config.holidayList;
+    this.lunar = config.lunar;
+  }
+
+  yearCalendar(
+    year: number = new Date().getUTCFullYear()
+  ) {
+    var result = [];
+    for (var i = 1; i < 13; i++) {
+      result.push(this.monthCalendar(year, i));
+    }
+
+    return result;
+  }
+
+  monthCalendar(
+    year: number = new Date().getUTCFullYear(), 
+    month: number = new Date().getUTCMonth()
+  ) {
+    // var dObj: dayjs.Dayjs = dayjs([year, month]);
+    var dObj: dayjs.Dayjs = dayjs(new Date(year, month - 1));
+    if (!dObj.isValid()) {
+      return null;
+    }
+
+    var mArr = [],
+        count = 1,
+        firstDayOfWeek = dObj.startOf('month').day(),
+        lDay = dObj.endOf("month").date();
+
+    while (count <= lDay) {
+      var wArr: Array<DateObjectType> = [];
+      if (count === 1) {
+        wArr = [ ...this.getFirstList(dObj, firstDayOfWeek) ];
+      } 
+      
+      for (var j = firstDayOfWeek; j < 7; j++) {
+        if (count <= lDay) {
+          var obj = this.buildDateObject(dObj, count++, j);
+          wArr.push(obj);
+        } else {
+          wArr = [ ...wArr, ...this.getLastList(dObj) ];
+          break;
+        }
+      }
+
+      firstDayOfWeek = 0;
+      mArr.push(wArr);
+    }
+
+    return mArr;
+  }
+
+  getFirstList(dObj: dayjs.Dayjs, fDay: number) {
+    var result = [];
+    var pDate = dObj.subtract(1, 'month');
+    for (var i = 0; i < fDay; i++) {
+      var date = pDate.endOf('month').date() - i,
+          dayOfWeek = (fDay - 1) - i,
+          obj = this.buildDateObject(pDate, date, dayOfWeek);
+
+      result.push(obj);
+    }
+
+    result.sort(function (a, b) {
+      if (a.dayOfWeek > b.dayOfWeek) return 1;
+      if (a.dayOfWeek < b.dayOfWeek) return -1;
+      return 0;
+    });
+
+    return result.sort();
+  }
+
+  getLastList(date: dayjs.Dayjs) {
+    var result = [],
+        dObj = date.add(1, 'month'),
+        firstDayOfNextDate = dObj.startOf('month').day();
+
+    var count = 1;
+    for (var i = firstDayOfNextDate; i < 7; i++) {
+      var obj = this.buildDateObject(dObj, count++, i);
+      result.push(obj);
+    }
+
+    return result;
+  }
+
+  buildDateObject(dObj: dayjs.Dayjs, date: number, dayOfWeek: number): DateObjectType {
+    var obj = {
+      year: dObj.year(),
+      month: dObj.month() + 1,
+      date: date,
+      dayOfWeek: dayOfWeek,
+      holidayName: "none",
+      fullDate: "",
+      lunarDate: ""
+    }
+
+    var tempObj = dayjs(new Date(obj.year, obj.month - 1, obj.date));
+    for (var i = 0; i < this.holidayList.length; i++) {
+      if (this.checkHoliday(this.holidayList[i], tempObj)) {
+        obj.holidayName = this.holidayList[i].dateName; 
+        break;
+      }
+    }
+
+    obj.fullDate = tempObj.format("YYYYMMDD");
+    obj.lunarDate = tempObj.lunar().format("YYYYMMDD");
+
+    return obj;
+  }
+
+  checkHoliday(holiday: HolidayType, dObj: dayjs.Dayjs) {
+    var tempDateObject = dObj;
+
+    if (holiday.bufferDay) {
+      var prevDate = tempDateObject.subtract(1, "days");
+      var nextDate = tempDateObject.add(1, "days");
+      if (this.matchHoliday(holiday, prevDate)) {
+        return true;
+      } else if (this.matchHoliday(holiday, nextDate)) {
+        return true;
+      } 
+    }
+
+    return this.matchHoliday(holiday, tempDateObject);
+  }
+
+  matchHoliday(holiday: HolidayType, dObj: dayjs.Dayjs) {
+    var tempDateObject = dObj;
+    var hDate = dayjs(new Date(dObj.year(), holiday.month - 1, holiday.date));
+    if (holiday.isLunar) {
+      tempDateObject = tempDateObject.lunar();
+    }
+    return tempDateObject.isSame(hDate);
+  }
+
+  yearHoliday(
+    year = new Date().getUTCFullYear()
+  ) {
+    var result = [];
+    var hList = this.holidayList;
+    for (var i = 0; i < hList.length; i++) {
+      var dObj = dayjs(new Date(year, hList[i].month - 1, hList[i].date));
+  
+      if (hList[i].isLunar) {
+        dObj = dObj.solar();
+      }
+  
+      if (hList[i].bufferDay) {
+        var prevDate = dObj.subtract(1, "days");
+        result.push(this.buildHolidayObject(prevDate, hList[i]));
+  
+        var nextDate = dObj.add(1, "days");
+        result.push(this.buildHolidayObject(nextDate, hList[i]));
+      }
+  
+      result.push(this.buildHolidayObject(dObj, hList[i]));
+    }
+    
+    return result;
+  }
+  
+  buildHolidayObject(dObj: dayjs.Dayjs, holiday: HolidayType) {
+    return {
+      year: dObj.year(),
+      month: dObj.month() + 1,
+      date: dObj.date(),
+      fullDate: dObj.format("YYYYMMDD"),
+      dateName: holiday.dateName
+    }
+  }
+
+}
+
+
+export = CalAPI;
